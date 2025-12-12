dc2911ee5b2a0f9af7f8c1fd3fd4d47678851ccd
soaud arziki <arziki.souad@gmail.com>
2025-12-12 04:49:20 +0100

    CU-869bdun30 la creation de authSlice

---
+++ gestion_flotte_frontend/features/authSlice.js
@@ -0,0 +1,115 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../src/services/axiosConfig";
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, motDePasse }, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/auth/login", { email, motDePasse });
+
+      const { token, data } = res.data;
+      console.log("logiiiin", res.data);
+      localStorage.setItem("token", res.token);
+      return { token, user: data };
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.mesage);
+    }
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+  try {
+    await api.post("/auth/logout");
+  } finally {
+    localStorage.removeItem("token");
+  }
+});
+
+export const getUserConnected =
+  ("auth/getUserConnected",
+  async (_, { rejectWithValue }) => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      return rejectWithValue("Pas de token");
+    }
+
+    try {
+      const res = await api.get("auth/me");
+      console.log("getuserconnect", res.data);
+      return { token, user: res.data.data };
+    } catch (error) {
+      localStorage.removeItem("token");
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  });
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    token: localStorage.getItem("token") || null,
+    isAuthenticated: false,
+    loading: false,
+    error: null,
+    isInitialized: false,
+  },
+  reducers: {
+    logoutLocal: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+      localStorage.removeItem('token');
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      // login actions
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = true;
+        state.token = action.payload.token;
+        state.user = action.payload.user;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      //logout
+      .addCase(logout.fulfilled, (state) => {
+        state.loading = false;
+        state.user = null;
+        state.token = null;
+        state.isAuthenticated = false;
+      })
+
+      //getUserConnected
+      .addCase(getUserConnected.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getUserConnected.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = true;
+        state.token = action.payload.token;
+        state.user = action.payload.user;
+        state.isInitialized = true;
+      })
+      .addCase(getUserConnected.rejected, (state) => {
+        state.loading = false;
+        state.isAuthenticated = false;
+        state.user = null;
+        state.token = null;
+        state.isInitialized = true;
+      });
+  },
+});
+
+export const { clearError, logoutLocal } = authSlice.actions;
+
+export default authSlice.reducer;
